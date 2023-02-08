a472beca2cc8dd0cf27a214964af5a1026b8e207
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-02-07 21:36:21 -0600

    Mostly done with each of the classes. I have added in the main functions for the index.js script. Works but I have not had a chance to fully debug.

--- index.js
+++ index.js
@@ -5,21 +5,151 @@ const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 
-// Creates a function to initialize app
-// init(){
-//     const manager = new Manager();
-//     manager.getName();
-// }
+// Constants/variables/objects 
+const employeeQuestions = [
+    {
+        type: 'input',
+        message: 'Name:',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'ID',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'Email:',
+        name: 'email',
+    },
+];
+const managerQuestions = [
+    {
+        type: 'input',
+        message: 'Name:',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'ID:',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'Email:',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Office Number:',
+        name: 'officeNumber',
+    },
+];
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: 'Name:',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'ID:',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'Email:',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'GitHub:',
+        name: 'github',
+    },
+];
+const internQuestions = [
+    {
+        type: 'input',
+        message: 'Name:',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'ID:',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'Email:',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'School:',
+        name: 'school',
+    },
+];
+const menuOptions = [
+    {
+        type: 'list',
+        message: 'Name:',
+        name: 'name',
+        choices: ['Engineer','Intern','Done']
+    },
+];
 
-const manager = new Manager();
-manager
-// manager.getName();
-// manager.getId();
-// manager.getEmail();
-// manager.getRole();
+// Creates a directory of all added employees
+var employees = [];
 
-//   myPromise
-    //  .then(console.log("SOMETHING"))
-//     .then(handleFulfilledB, handleRejectedB)
-//     .then(handleFulfilledC, handleRejectedC);
-  
+// on startup, finds the information for the team manager
+teamManager();
+
+// Functions
+function teamManager() {
+    inquirer
+    .prompt(managerQuestions)
+    .then((data) => {
+        const manager = new Manager(data.name, data.id, data.email, data.officeNumber)
+        employees[0] = manager;
+        menu()
+    })
+    
+};
+
+function menu(){
+    console.log(employees)
+    inquirer
+    .prompt(menuOptions)
+    .then((data) => {
+        console.log(data)
+        if(data.name=='Engineer'){
+            addEmployee(engineerQuestions,'Engineer')
+        } else if (data.name=='Intern'){
+            addEmployee(internQuestions,'Intern')
+        } else {
+            writeFile()
+        }
+    })
+}
+
+function addEmployee(questions,employee){
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        if(employee=='Engineer'){
+            const engineer = new Engineer(data.name, data.id, data.email, data.github)
+            employees.push(engineer)
+            menu()
+        } else if (employee=='Intern'){
+            const intern = new Intern(data.name, data.id, data.email, data.school)
+            employees.push(intern)
+            menu()
+        } else {
+            console.log('error')
+        }
+    })
+}
+
+function writeFile(){
+    console.log("Done!")
+}
