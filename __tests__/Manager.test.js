93dbddd34ca5385205626bb23b72b78e89a9b19d
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-02-09 22:40:48 -0600

    Completed the manager tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,12 +2,46 @@ const Manager = require('../lib/Manager');
 
 describe('Manager', () => {
 
+    describe("Initialization", () => {
+        it("Sets the class properties", () => {
+            const manager = new Manager("name","id","email","officeNumber");
+            expect(manager.officeNumber).toEqual("officeNumber");
+        })
+        
+    });
+
+    describe('getName', () => {
+        it("When calling 'getName()' the value of 'name' should be returned", () => {
+            const manager = new Manager("name","id","email","officeNumber");
+            expect(manager.getName()).toEqual("name");
+        })
+        
+    });
+
+    describe('getId', () => {
+        it("When calling 'getId()' the value of 'id' should be returned", () => {
+            const manager = new Manager("name","id","email","officeNumber");
+            expect(manager.getId()).toEqual("id");
+        })
+        
+    });
+
+    describe('getEmail', () => {
+        it("When calling 'getEmail()' the value of 'email' should be returned", () => {
+            const manager = new Manager("name","id","email","officeNumber");
+            expect(manager.getEmail()).toEqual("email");
+        })
+        
+    });
+
     describe('getRole', () => {
         it("When calling 'getRole()' the value of 'Manager' should be returned", () => {
-            const manager = new Manager()
-            expect(manager.getRole()).toEqual("Manager")
+            const manager = new Manager("name","id","email","officeNumber");
+            expect(manager.getRole()).toEqual("Manager");
         })
         
     });
 });
+
+
 
