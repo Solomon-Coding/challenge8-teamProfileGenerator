d5d92e59a54a47cd7e5bb9b745635e0de3d87c9f
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-02-09 22:26:05 -0600

    completed the employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,15 +2,44 @@ const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
 
-    // const employee = new Employee("bob",1,"email")
-    
-    // // Assert
-    // expect(employee.name).toEqual("bob"));
+    describe("Initialization", () => {
+        it("When calling 'getRole()' the value of 'Employee' should be returned", () => {
+            const employee = new Employee("name","id","email");
+            expect(employee.name).toEqual("name");
+            expect(employee.id).toEqual("id");
+            expect(employee.email).toEqual("email");
+        })
+        
+    });
+
+    describe('getName', () => {
+        it("When calling 'getName()' the value of 'name' should be returned", () => {
+            const employee = new Employee("name","id","email");
+            expect(employee.getName()).toEqual("name");
+        })
+        
+    });
+
+    describe('getId', () => {
+        it("When calling 'getId()' the value of 'id' should be returned", () => {
+            const employee = new Employee("name","id","email");
+            expect(employee.getId()).toEqual("id");
+        })
+        
+    });
+
+    describe('getEmail', () => {
+        it("When calling 'getEmail()' the value of 'email' should be returned", () => {
+            const employee = new Employee("name","id","email");
+            expect(employee.getEmail()).toEqual("email");
+        })
+        
+    });
 
     describe('getRole', () => {
         it("When calling 'getRole()' the value of 'Employee' should be returned", () => {
-            const employee = new Employee()
-            expect(employee.getRole()).toEqual("Employee")
+            const employee = new Employee("name","id","email");
+            expect(employee.getRole()).toEqual("Employee");
         })
         
     });
